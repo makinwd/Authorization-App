6e6aa7ac1628fe24b5fded160d480c626f2f0113
pustot <maki.nwd@gmail.com>
2023-02-26 22:17:22 +0300

    add form and button

--- src/pages/auth-page/authForm/authForm.tsx
+++ src/pages/auth-page/authForm/authForm.tsx
@@ -1,11 +1,50 @@
 import React, { FC } from 'react';
-import Typography from '@mui/material/Typography' 
-import styles from './authForm.module.css'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import styles from './authForm.module.css';
+import { TextField } from '@mui/material';
+
 
 export const AuthForm: FC = () => {
-    return (
-        <div className={styles.authForm}>
-            <Typography variant='h4' component='div'>Войдите</Typography>
-        </div>
-    )
-}
+  return (
+    <div className={styles.authForm}>
+      <Typography variant="h4" component="div">
+        Войдите
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        component="div"
+        gutterBottom={true}
+        className={styles.authForm__subtitle}
+      >
+        Если ранее регистрировались
+      </Typography>
+      <form className={styles.authForm__form}>
+        <TextField
+          label="Логин"
+          size="small"
+          margin="normal"
+          className={styles.authForm__input}
+          fullWidth={true}
+        />
+        <TextField
+          label="Пароль"
+          type="password"
+          size="small"
+          margin="normal"
+          className={styles.authForm__input}
+          fullWidth={true}
+        />
+        <Button
+          type="submit"
+          variant="outlined"
+          fullWidth={true}
+          disableElevation={true}
+          sx={{ marginTop: 2 }}
+        >
+          Войти
+        </Button>
+      </form>
+    </div>
+  );
+};
